ee5d206c5c52f6058d0ec115181c33f39ecbd75b
azechi <azechi.n@gmail.com>
2020-03-01 11:11:21 +0900

    .

--- scripts/viewer.js
+++ scripts/viewer.js
@@ -1,51 +1,48 @@
 import { LANPeerConnection } from "./lanpeerconnection.js";
 import { buildHubConnection } from "./hubconnection.js";
 
-const stream = new MediaStream();
-let message;
-let p_status;
+import { getRefreshToken, getIdToken } from "./viewer_login.js";
 
-window.onload = () => {
-  document.getElementById("video").srcObject = stream;
-  message = document.getElementById("message");
-  p_status = document.getElementById("status");
-};
+const key = "Refresh Token";
+const storage = window.localStorage;
+let rt = storage.getItem(key);
+
+const contentLoaded = new Promise(resolve => {
+  if (document.readyState == 'loading') {
+    document.addEventListener('domcontentloaded', resolve);
+  } else {
+    resolve();
+  }
+});
 
 (async () => {
-  // get idToken
-  //device authorization flow
-  // auth0
-  const idToken = await getIdToken({
-    baseUrl: "https://azechify.auth0.com/oauth/",
-    clientId: "18cBqG2qRvzXvxdGrMamVpVL3zB9b1tn",
-    callback: (user_code, verification_uri, verification_uri_complete) => {
-      console.log(user_code, verification_uri, verification_uri_complete);
-      [user_code, verification_uri].forEach(s => {
-        const p = document.createElement("p");
-        p.innerText = s;
-        // await document.load
-        message.appendChild(p);
-      });
 
-      const a = document.createElement("a");
-      a.href = verification_uri_complete;
-      a.innerText = verification_uri_complete;
-      message.appendChild(a);
-    }
-  });
+  if (!rt) {
+    rt = await getRefreshToken(code => contentLoaded.then(() => console.log(code)));
+    storage.setItem(key, rt);  
+  }
 
-  [...message.children].map(e => e.remove());
-  p_status.innerText = "hub connection ...";
+  const idToken = await getIdToken(rt);
 
   const { hub, send: sendToHub } = await buildHubConnection({
     serviceUrl: new URL("https://p1-azechify.azure-api.net/"),
     idTokenFactory: () => idToken
   });
 
-  let flg = false;
+  await contentLoaded;
+
+  const stream = new MediaStream();
+  document.getElementById('video').srcObject = stream;
 
   const connected = new Promise(resolve => {
     const pc = new LANPeerConnection();
+
+    pc.ontrack = e => {
+      stream.addTrack(e.track);
+    };
+
+    let flg = false;
+
     pc.send = async sessionDescription => {
       // polling
       while (!flg) {
@@ -59,15 +56,10 @@ window.onload = () => {
             }
           ]
         });
-        p_status.innerText = "offerした";
         await new Promise(r => setTimeout(r, 2000));
       }
     };
 
-    pc.ontrack = e => {
-      stream.addTrack(e.track);
-      p_status.innerText = "";
-    };
 
     const h = () => {
       if (pc.connectionState == "connected") {
@@ -84,74 +76,16 @@ window.onload = () => {
 
     hub.on("answer", async ({ from, sessionDescription }) => {
       flg = true;
-      p_status.innerText = "answer 来た";
-      console.log("master id", from);
       pc.setRemoteDescription(sessionDescription);
     });
 
     hub.start().then(() => {
-      p_status.innerText = "hub connected";
       pc.createSignalingDataChannel();
     });
   });
 
-  p_status.innerText = "hub connecting ...";
   const pc = await connected;
-  p_status.innerText = "connected!";
 
   hub.stop();
 })();
 
-async function getIdToken({ baseUrl, clientId, callback }) {
-  let p = new URLSearchParams();
-  p.set("client_id", clientId);
-  p.set("scope", "openid offline_access");
-  //p.set('audience', audience);
-
-  let data = await fetch(baseUrl + "device/code", {
-    mode: "cors",
-    method: "POST",
-    body: p
-  }).then(res => res.json());
-
-  // display
-  // data.user_code
-  // data.verification_uri
-  // data.verification_uri_complete: verification_uri + user_code
-  // data.expires_in(seconds)
-  callback(
-    data.user_code,
-    data.verification_uri,
-    data.verification_uri_complete
-  );
-
-  // polling
-  // {data.device_code, data.interval(seconds)}
-
-  p = new URLSearchParams();
-  p.set("client_id", clientId);
-  p.set("device_code", data.device_code);
-  p.set("grant_type", "urn:ietf:params:oauth:grant-type:device_code");
-
-  while (true) {
-    let response = await fetch(baseUrl + "token", {
-      mode: "cors",
-      method: "POST",
-      body: p
-    });
-
-    if (response.ok) {
-      return (await response.json()).id_token;
-    }
-
-    if (
-      response.status == 403 &&
-      (await response.json()).error == "authorization_pending"
-    ) {
-      await new Promise(resolve => setTimeout(resolve, data.interval * 1000));
-      continue;
-    }
-
-    throw await response.json();
-  }
-}
---
+++ scripts/viewer_login.js
@@ -0,0 +1,88 @@
+export { getRefreshToken, getIdToken };
+
+const domain = "azechify.auth0.com";
+const clientId = "18cBqG2qRvzXvxdGrMamVpVL3zB9b1tn";
+
+/*
+displayCallback({
+  user_code,
+  verification_uri,
+  verification_uri_complete
+})
+*/
+async function getRefreshToken(displayCallback) {
+  const deviceCodeResponse = await getDeviceCode("offline_access openid");
+
+  displayCallback(deviceCodeResponse);
+
+  return (await getTokenUsingDeviceCode(deviceCodeResponse)).refresh_token;
+}
+
+async function getIdToken(refreshToken) {
+  return (await getTokenUsingRefreshToken(refreshToken)).id_token;
+}
+
+
+async function getTokenUsingRefreshToken(refreshToken) {
+  const response = await callApi("token", {
+    grant_type: "refresh_token",
+    refresh_token: refreshToken
+  });
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw await response.json();
+}
+
+async function getDeviceCode(scope) {
+  const response = await callApi("device/code", {
+    scope
+  });
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw await response.json();
+}
+
+async function getTokenUsingDeviceCode(deviceCodeResponse) {
+  while (true) {
+    const response = await callApi("token", {
+      device_code: deviceCodeResponse.device_code,
+      grant_type: "urn:ietf:params:oauth:grant-type:device_code"
+    });
+
+    if (response.ok) {
+      return response.json();
+    }
+
+    if (
+      response.status == 403 &&
+      (await response.json()).error == "authorization_pending"
+    ) {
+      await new Promise(resolve =>
+        setTimeout(resolve, deviceCodeResponse.interval * 1000)
+      );
+      continue;
+    }
+
+    throw await response.json();
+  }
+}
+
+
+// new URLSearchParams()
+// fetch()
+function callApi(endpointPath, data) {
+  return fetch(`https://${domain}/oauth/${endpointPath}`, {
+    mode: "cors",
+    method: "POST",
+    body: new URLSearchParams({
+      client_id: clientId,
+      ...data
+    })
+  });
+}
