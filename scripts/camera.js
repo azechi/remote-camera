11f258ce00388929c7340a12118e4e3dc04b17db
azechi <azechi.n@gmail.com>
2020-03-19 10:50:40 +0900

    ,

--- scripts/camera.js
+++ scripts/camera.js
@@ -2,6 +2,30 @@ import auth0 from "./login.js";
 import { LANPeerConnection } from "./lanpeerconnection.js";
 import { buildHubConnection } from "./hubconnection.js";
 
+async function loop(context, handlers, initialLabel) {
+  const listen = async function*() {
+    let resolve;
+
+    function emit(...args) {
+      resolve(...args);
+    }
+
+    yield [initialLabel, context, emit];
+    while (true) {
+      yield [await new Promise(rslv => (resolve = rslv)), context];
+    }
+  };
+
+  const _handlers = new Map(handlers);
+
+  for await (const [label, context, emit] of listen()) {
+    if (!_handlers.has(label)) {
+      throw `"${label}" handler not found`;
+    }
+    await _handlers.get(label)(context, emit);
+  }
+}
+
 const contentLoaded = new Promise(resolve => {
   const f = () => {
     resolve({
@@ -24,36 +48,36 @@ const contentLoaded = new Promise(resolve => {
 });
 
 const data = (() => {
-  let connection = new RTCPeerConnection();
-  let stream = new MediaStream();
+  let _pc = new RTCPeerConnection();
+  let _stream = new MediaStream();
 
-  return {};
-})();
-
-async function loop(context, handlers, initialLabel) {
-  const listen = async function*() {
-    let resolve;
-
-    function emit(...args) {
-      resolve(...args);
-    }
-
-    yield [initialLabel, context, emit];
-    while (true) {
-      yield [await new Promise(rslv => (resolve = rslv)), context];
+  return {
+    get pc() {
+      return _pc;
+    },
+    set pc(val) {
+      _pc = val;
+      new Promise(r => setTimeout(r, 1000)).then(_ => {
+        _stream.getTracks().forEach(track => {
+          console.log(track);
+          _pc.addTransceiver(track, { stream: _stream, direction: "sendonly" });
+        });
+      });
+    },
+    get stream() {
+      return _stream;
+    },
+    set stream(val) {
+      _stream = val;
+      _pc.getSenders().forEach(sender => {
+        const track = _stream
+          .getTracks()
+          .find(track => track.kind == sender.track.kind);
+        sender.replaceTrack(track);
+      });
     }
   };
-
-  const _handlers = new Map(handlers);
-
-  for await (const [label, context, emit] of listen()) {
-    if (!_handlers.has(label)) {
-      throw `"${label}" handler not found`;
-    }
-    console.log(label);
-    await _handlers.get(label)(context, emit);
-  }
-}
+})();
 
 /* connection */
 (async () => {
@@ -89,10 +113,11 @@ async function loop(context, handlers, initialLabel) {
     ctx.button.textContent = "disconnect";
 
     /* fire and forget */
-    viewerConnected(() => ctx.writeStatus("pending")).then(pc => {
-      ctx.writeStatus("connected");
-      console.log("connected", pc);
-    });
+    /* キャンセル可能にする必要がある */
+    viewerConnected(
+      () => ctx.writeStatus("pending"),
+      () => ctx.writeStatus("connected")
+    ).then(pc => (data.pc = pc));
   };
 
   const disconnectHandler = ctx => {
@@ -100,11 +125,8 @@ async function loop(context, handlers, initialLabel) {
     ctx.button.disabled = false;
     ctx.button.dataset.command = "connect";
     ctx.button.textContent = "connect";
-    /* 
-    if (pc) {
-      pc.disconnect();
-    }
-    */
+
+    data.pc = null;
   };
 
   loop(
@@ -133,8 +155,6 @@ const defaultVideoTrackConstraints = {
 (async () => {
   const { document, mediaDevices } = await contentLoaded;
 
-  let _stream = new MediaStream();
-
   const context = {
     btn_start: document.getElementById("button_stream"),
     btn_videoMute: document.getElementById("button_videoTrack"),
@@ -142,10 +162,10 @@ const defaultVideoTrackConstraints = {
     btn_show: document.getElementById("button_preview"),
     video: document.getElementById("video"),
     get stream() {
-      return _stream;
+      return data.stream;
     },
     set stream(val) {
-      _stream = val;
+      data.stream = val;
       this.video.srcObject = !(this.video.dataset.alt == "true") ? val : null;
     }
   };
@@ -192,7 +212,6 @@ const defaultVideoTrackConstraints = {
     ctx.btn_audioMute.disabled = false;
   };
 
-  //stream.getTracks().forEach(t => t.stop());
   const stop = ctx => {
     ctx.stream.getTracks().forEach(track => track.stop());
     ctx.btn_start.dataset.alt = false;
@@ -213,37 +232,10 @@ const defaultVideoTrackConstraints = {
   );
 })();
 
-/*
-(async () => {
-
-  let stream = new MediaStream();
-  let pc;
-
-  const view = await contentLoaded;
-
-  // fire and forget
-  (async () => {
-    for await (const s of spawnStream()) {
-      stream.getTracks().forEach(t => t.stop());
-      stream = s;
-    }
-  })();
-
-  view.streamActivate.addEventListener("click", async ({currentTarget:e})=>{
-    if (stream && stream.active) {
-      stream.getTracks().forEach(t => t.stop());
-    } else {
-      const s = await navigator.mediaDevices.getUserMedia(defaultUserMediaConstraints);
-      await new Promise(r => setTimeout(r, 1000));
-      s.getVideoTracks()[0].applyConstraints(defaultVideoTrackConstraints);
-      _spawnResolve(s);
-    }
-  });
-
-})();
-*/
-
-const viewerConnected = async hubConnectedCallback => {
+const viewerConnected = async (
+  hubConnectedCallback,
+  viewerConnectedCallback
+) => {
   const auth = ($dbg.auth = await auth0);
 
   const { hub, send: sendToHub } = await buildHubConnection({
@@ -258,6 +250,7 @@ const viewerConnected = async hubConnectedCallback => {
       const onConnected = () => {
         if (pc.connectionState == "connected") {
           pc.removeEventListener("connectionstatechange", onConnected);
+          viewerConnectedCallback();
           resolve(pc);
         }
       };
@@ -283,46 +276,4 @@ const viewerConnected = async hubConnectedCallback => {
   await hub.start();
   hubConnectedCallback();
   return connected;
-};
-
-// main
-async () => {
-  const stream = ($dbg.stream = await navigator.mediaDevices.getUserMedia(
-    defaultUserMediaConstraints
-  ));
-
-  // https://crbug.com/711524
-  await new Promise(r => setTimeout(r, 1000));
-
-  await stream
-    .getVideoTracks()[0]
-    .applyConstraints(defaultVideoTrackConstraints);
-
-  window.addEventListener("hashchange", async e => {
-    console.log("hashchanged", p);
-    await stream.getVideoTracks()[0].applyConstraints(p);
-  });
-
-  await contentLoaded;
-
-  const elem = {
-    video: document.getElementById("video")
-  };
-
-  elem.video.addEventListener("loadedmetadata", () => {
-    const s = `width:${elem.video.videoWidth}, height:${elem.video.videoHeight}`;
-    console.log(s);
-  });
-
-  elem.video.srcObject = stream;
-  const viewer = await viewerConnected;
-
-  // peerconnection connectedの直後にaddTransceiverしてもnegotiationneededが発火しないから1秒待つ
-  await new Promise(r => setTimeout(r, 1000));
-
-  stream
-    .getTracks()
-    .forEach(track =>
-      viewer.addTransceiver(track, { stream, direction: "sendonly" })
-    );
 };
